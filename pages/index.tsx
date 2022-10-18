528e1a08f4cc5b634772e1a3ab1a802decbab97c
Sean OReilly <soreilly424@gmail.com>
2022-10-18 11:06:04 -0600

    added prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,11 @@ import Layout from '@/components/Layout'
 import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
-  return <Layout>Hello World</Layout>
+  return (
+    <Layout>
+      <h1 className="text-sm md:text-xl lg:text-3xl">Hello World</h1>
+    </Layout>
+  )
 }
 
 export default Home
---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  plugins: [require('prettier-plugin-tailwindcss')],
+  tailwindConfig: './tailwind.config.js',
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+}
