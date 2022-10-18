be35208c6678b10058043e928013fa06eafca13d
Sean OReilly <soreilly424@gmail.com>
2022-10-18 10:52:36 -0600

    adding tailwind plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,21 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    debugScreens: {
+      position: ['bottom', 'right'],
+      style: {
+        backgroundColor: '#262626e6',
+        borderRadius: '4px',
+        boxShadow: 'none',
+        color: 'white',
+        padding: '8px',
+        margin: '18px 16px',
+        opacity: '100%',
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    require('tailwindcss-debug-screens'),
+    require('@tailwindcss/typography'),
+  ],
 }
