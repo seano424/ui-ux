01f3e55cbb1e1a2cc531ec7b042143f5ed7bdb57
Sean OReilly <soreilly424@gmail.com>
2022-10-18 10:54:56 -0600

    updating tsconfig

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>hello world</main>
+      <main></main>
       <footer></footer>
     </div>
   )
