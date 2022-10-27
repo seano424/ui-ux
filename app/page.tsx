8ccd7d59012cac57cb5b808fc16f6d76045dc3a4
Sean OReilly <soreilly424@gmail.com>
2022-10-27 14:49:04 -0600

    added first page

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,13 @@
-import Layout from '@/components/Layout'
 import type { NextPage } from 'next'
 import Button from '@/components/Button/Button'
 import Skeleton from '@/components/Skeleton/Skeleton'
 
 const Home: NextPage = () => {
   return (
-    <Layout>
-      <article className="container space-y-10 py-10">
-        <Button label="My Button" />
-        <Skeleton />
-      </article>
-    </Layout>
+    <div className="container space-y-10 py-10">
+      <Button label="My Button" />
+      <Skeleton />
+    </div>
   )
 }
 
