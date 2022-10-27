c96617dec12b55c40f9f6f4953784eb37a9bbb2d
Sean OReilly <soreilly424@gmail.com>
2022-10-27 14:50:02 -0600

    updating skeleton colors

--- components/Skeleton/Skeleton.tsx
+++ components/Skeleton/Skeleton.tsx
@@ -6,23 +6,23 @@ function Skeleton() {
       relative isolate 
       space-y-5 overflow-hidden 
       rounded-2xl border-8
-      border-rose-100/10
+      border-black/10
       bg-white/5 p-4
       shadow-xl
       shadow-black/5
       before:absolute
       before:inset-0 before:-translate-x-full before:animate-[shimmer_2s_infinite]
       before:border-t
-      before:border-rose-100/10
+      before:border-black/10
       before:bg-gradient-to-r before:from-transparent
-      before:via-rose-100/10 before:to-transparent
+      before:via-black/10 before:to-transparent
       "
     >
-      <div className="h-24 rounded-lg bg-rose-100/10"></div>
+      <div className="h-24 rounded-lg bg-black/10"></div>
       <div className="space-y-3">
-        <div className="h-3 w-3/5 rounded-lg bg-rose-100/10"></div>
-        <div className="h-3 w-4/5 rounded-lg bg-rose-100/20"></div>
-        <div className="h-3 w-2/5 rounded-lg bg-rose-100/20"></div>
+        <div className="h-3 w-3/5 rounded-lg bg-black/10"></div>
+        <div className="h-3 w-4/5 rounded-lg bg-black/20"></div>
+        <div className="h-3 w-2/5 rounded-lg bg-black/20"></div>
       </div>
     </div>
   )
